6408d44a2780bc42b59904e297026b315ee34448
YasiruShaminda <yasiru.20200496@iit.ac.lk>
2022-02-25 02:00:58 +0530

    developing mid method

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def get_data(file):
             for x in range(int(conNum)):
                 count += 1
                 skillDict = {}
+                conTime = 0
                 conName = lines[count].strip().split()[0]
                 conSkillCount = lines[count].strip().split()[1]
 
@@ -26,7 +27,7 @@ def get_data(file):
 
                     skillDict[conSkill] = conSkillLvl
 
-                conDict[conName] = skillDict
+                conDict[conName] = [conTime, skillDict]
 
             print(count)
             print(conDict)
@@ -54,12 +55,52 @@ def get_data(file):
 
             f.close()
 
+        return conDict,proDict
+
     except FileNotFoundError:
         return "File not found"
 
 
+def set_contributors():
+    global proDict
+    global conDict
+
+    for projectName in proDict:
+        setProj = []
+        project = proDict[projectName]
+        setProj.append(projectName)
+
+        for projectSkill in project[3]:
+            skill = projectSkill
+            skillLvl = project[3].get(projectSkill)
+
+            for conName in conDict:
+                conSkills = conDict[conName]
+
+                for conSkill in conSkills[1]:
+                    if skillLvl < conSkills[1].get(conSkill) and conSkills[0] <= project[0]+conSkills[0]:
+                        setProj.append(conName)
+                        conSkills[0] = project[0] + conSkills[0]
+                    elif skillLvl == conSkills[1].get(conSkill) and conSkills[0] <= project[0]+conSkills[0]:
+                        setProj.append(conName)
+                        conSkills[0] = project[0] + conSkills[0]
+                        conSkills[conSkill] = conSkills.get(conSkill) + 1
+                    elif skillLvl - 1 == conSkills[1].get(conSkill):
+                        for conName2 in conDict:
+                            conSkills2 = conDict[conName2]
+
+                            for conSkill2 in conSkills2[1]:
+                                if skillLvl < conSkills2[1].get(conSkill2) and conSkills[0] <= project[0]+conSkills[0]:
+                                    setProj.append(conName)
+                                    setProj.append(conName2)
+            print(setProj)
+
+
 file = "a_an_example.in.txt"
-conNum = 0
-proNum = 0
-get_data(file)
-print("------")
+# conNum = 0
+# proNum = 0
+
+conDict, proDict = get_data(file)
+print("Data Retrieved\n-------------\n")
+
+set_contributors()
